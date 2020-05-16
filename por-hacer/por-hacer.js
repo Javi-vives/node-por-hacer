f48dd99611a917d4d0c0260c82c0d9b3105ee637
Javi <javi13core@hotmail.com>
2020-05-17 00:26:40 +0200

    Primer commit

---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,90 @@
+const fs = require('fs');
+
+let listadoPorHacer = [];
+
+const guardarDB = () => {
+
+    let data = JSON.stringify(listadoPorHacer);
+
+    fs.writeFile('db/data.json', data, (err) => {
+        if (err) throw new Error('No se puedo grabar', err);
+    })
+}
+
+
+const cargarDB = () => {
+
+    try {
+        listadoPorHacer = require('../db/data.json');
+    } catch (error) {
+        listadoPorHacer = [];
+    }
+}
+
+
+const crear = (descripcion) => {
+
+    cargarDB();
+
+    let porHacer = {
+        descripcion,
+        completado: false
+    };
+
+    listadoPorHacer.push(porHacer);
+
+    guardarDB();
+    return porHacer;
+}
+
+const getListado = () => {
+    cargarDB();
+    return listadoPorHacer;
+}
+
+const actualizar = (descripcion, completado = true) => {
+
+    cargarDB();
+
+    //Hace un recorrido por todo el array encontrando el index de la descripcion pasada
+    // tarea puede ser otro nombre cualquiera
+    let index = listadoPorHacer.findIndex(tarea => {
+        return tarea.descripcion === descripcion;
+    })
+
+    if (index >= 0) {
+        listadoPorHacer[index].completado = completado;
+        guardarDB();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const borrar = (descripcion) => {
+
+    cargarDB();
+
+    //Filtra el array y devuelve otro array de la descripcion pasada,
+    // tarea puede ser otro nombre cualquiera
+    let nuevoListado = listadoPorHacer.filter(tarea => {
+        return tarea.descripcion !== descripcion;
+    })
+
+    // Si tienen la misma longitud es que no se ha borrado nada
+    if (listadoPorHacer.length === nuevoListado.length) {
+        return false;
+    } else {
+        listadoPorHacer = nuevoListado;
+        guardarDB();
+        return true;
+    }
+
+}
+
+module.exports = {
+    crear: crear,
+    getListado: getListado,
+    actualizar: actualizar,
+    borrar: borrar
+}
